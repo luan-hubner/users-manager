84a1d4c782423787b00878241697153ecddc7b97
luan-hubner <luanhubner.pro@gmail.com>
2022-01-28 00:06:56 -0400

    feat: add input component

---
+++ src/components/InputText/index.tsx
@@ -0,0 +1,42 @@
+import { InputAdornment, OutlinedInputProps } from '@mui/material';
+import { alpha, styled } from '@mui/material/styles';
+
+import TextField, { TextFieldProps } from '@mui/material/TextField';
+
+import Lock from '@mui/icons-material/Lock';
+import Mail from '@mui/icons-material/Mail';
+
+const InputText = styled((props: TextFieldProps) => (
+  <TextField
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiFormControl-root': {
+    width: '100%',
+  },
+  '& .MuiFilledInput-root': {
+    overflow: 'hidden',
+    borderRadius: 4,
+    height: '65px',
+    padding: '0 1rem 0 .4rem',
+    backgroundColor: 'var(--bg-dark)',
+    '&.Mui-focused': {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+      borderColor: 'var(--bg-light);',
+      color: 'black'
+    },
+  },
+  '& .MuiInputLabel-root': {
+    fontFamily: 'var(--archivo-li)',
+    color: 'var(--input-text-color)',
+    fontSize: '1.5rem',
+    width: '100%',
+    padding: '0 .5rem',
+  },
+  '& .MuiFilledInput-input': {
+    fontFamily: 'var(--archivo-b)',
+    color: 'var(--input-text-color)'
+  },
+}));
+
+export { InputText };
