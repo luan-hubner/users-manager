f6d09d9c3f6bb239a4b1c711bd28bfb0ea61fb51
luan-hubner <luanhubner.pro@gmail.com>
2022-01-31 00:34:19 -0400

    add menu to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,99 @@
+import { useState } from 'react';
+
 import styles from './styles.module.css';
 
 import users__manager from '../../assets/images/usersmanager.png';
 import user__photo from '../../assets/images/me.png';
 
+import Person from '@mui/icons-material/Person';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import Logout from '@mui/icons-material/Logout';
+import Tooltip from '@mui/material/Tooltip';
+
 export default function Header() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <header>
       <div className={styles.container}>
         <div className={styles.content}>
           <img src={users__manager} alt="users-manager" />
 
-          <span>
-            <img src={user__photo} alt="user" />
-          </span>
+          <Tooltip title="Opções da Conta">
+            <span>
+              <img
+                src={user__photo}
+                alt="user"
+                onClick={handleClick}
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+              />
+            </span>
+          </Tooltip>
         </div>
+        <Menu
+          id="account-menu"
+          className={styles.account__menu}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&:before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 2,
+                right: 25,
+                width: 10,
+                height: 10,
+                bgcolor: 'var(--bg-ultra-dark)',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+          >
+          <MenuItem>
+            <ListItemIcon>
+              <Person fontSize="medium" className={styles.menu__icon} />
+            </ListItemIcon>
+            Meu Perfil
+          </MenuItem>
+          <Divider />
+          <MenuItem>
+            <ListItemIcon>
+              <Logout fontSize="medium" className={styles.menu__icon} />
+            </ListItemIcon>
+            Sair
+          </MenuItem>
+        </Menu>
       </div>
     </header>
   );
