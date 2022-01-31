6eaee8c4dc0102c2937f05a4d02b28492f6b8d94
Luan Hubner <luanhubner.pro@gmail.com>
2022-01-31 11:36:26 -0400

    add delete user modal component

---
+++ src/components/DeleteUserModal/index.tsx
@@ -0,0 +1,69 @@
+import styles from './styles.module.css';
+
+import { Modal } from "@mui/material";
+
+import Close from '@mui/icons-material/Close';
+import CButton from '../Button';
+
+type DeleteUserModalProps = {
+  setDeleteUserModalOpened: (value: boolean) => void;
+  user: UserType;
+};
+
+type UserType = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  email: string;
+  document: string;
+  password: string;
+  role: string;
+  photo: string;
+}
+
+export default function DeleteUserModal({ setDeleteUserModalOpened, user }: DeleteUserModalProps) {
+  async function removeUser() {
+    console.log(user.id)
+  };
+
+  return (
+    <Modal
+      open={true}
+      onClose={() => setDeleteUserModalOpened(false)}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      <div className={styles.delete__user__modal}>
+        <div className={styles.modal__body}>
+          <div className={styles.modal__header}>
+            <Close
+              className={styles.icon}
+              onClick={() => setDeleteUserModalOpened(false)}
+            />
+          </div>
+
+          <div className={styles.title}>
+            <h1>deseja realmente remover o usuário <span>{user.firstName} {user.lastName}</span>?</h1>
+          </div>
+
+          <div className={styles.buttons}>
+            <div onClick={() => removeUser()}>
+              <CButton
+                color="success"
+                label="CONFIRMAR"
+                type="submit"
+              />
+            </div>
+            <div onClick={() => setDeleteUserModalOpened(false)}>
+              <CButton
+                color="error"
+                label="CANCELAR"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+}
